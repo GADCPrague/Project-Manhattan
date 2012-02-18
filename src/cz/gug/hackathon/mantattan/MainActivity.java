d6d57c7c68940db20f9484e64b92a4cd9e2c384c
Jan Zalsky <zalsky@computerhelp.cz>
2012-02-18 15:56:23 +0100

    about screen and menu - draft (no graphics)

--- src/cz/gug/hackathon/mantattan/MainActivity.java
+++ src/cz/gug/hackathon/mantattan/MainActivity.java
@@ -1,6 +1,7 @@
 package cz.gug.hackathon.mantattan;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -15,11 +16,19 @@ public class MainActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
         
-        final Button button = (Button) findViewById(R.id.start_new_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button start = (Button) findViewById(R.id.start_new_button);
+        start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	Toast toast = Toast.makeText(v.getContext(), "start", Toast.LENGTH_LONG);
             	toast.show();
+            }
+        });
+        
+        final Button about = (Button) findViewById(R.id.about_button);
+        about.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = new Intent(v.getContext(), AboutActivity.class);
+            	startActivity(intent);
             }
         });
         
