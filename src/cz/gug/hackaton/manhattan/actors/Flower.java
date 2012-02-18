2adde964c2a7120ba96f408d84364b39de4b911a
unknown <Lzu@Lzu-HP.(none)>
2012-02-18 17:48:01 +0100

    flower changes

--- src/cz/gug/hackaton/manhattan/actors/Flower.java
+++ src/cz/gug/hackaton/manhattan/actors/Flower.java
@@ -1,8 +1,13 @@
 package cz.gug.hackaton.manhattan.actors;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Picture;
+import android.graphics.Rect;
+import android.graphics.RectF;
 
-public class Flower extends PlantHolder{
+public class Flower extends PlantHolder implements Renderable {
 		
 	private Picture flowerHead;
 	private Picture flowerBody;
@@ -23,14 +28,109 @@ public class Flower extends PlantHolder{
 			      Picture flowerLeaf2,Picture cursor,Picture background ) {
 		
 		super(xpos, ypos, holder_width, holder_height);
-		this.flowerHead = flowerHead;
-		this.flowerBody = flowerBody;
-		this.flowerLeaf1 = flowerLeaf1;
-		this.flowerLeaf2 = flowerLeaf2;
-		this.cursor = cursor;
-		this.background = background;
+	
+		
+		// init dummy pictures
+		
+		//flower head
+		Canvas fc1 = flowerHead.beginRecording(60, 60);
+		Paint fp1 = new Paint();
+		fp1.setColor(Color.RED);
+		fc1.drawCircle(30, 30, 15, fp1);
+		for (int i = 0; i < 8; i++) {
+			fc1.drawCircle((float)(30+Math.cos(i*Math.PI/4)*15), 
+					       (float)(30+Math.sin(i*Math.PI/4)*15), 5, fp1);
+		}
+		flowerHead.endRecording();
+		
+		//flower body
+		fc1 = flowerBody.beginRecording(5, 30);
+		fp1.setColor(Color.GREEN);
+		fc1.drawRect(new Rect(0,0,3,30), fp1);
+		flowerBody.endRecording();
+		
+		//flowerLeaf1
+		fc1 = flowerLeaf1.beginRecording(15, 15);
+		fc1.drawCircle(7, 7, 7, fp1);
+		flowerLeaf1.endRecording();
+		
+		//flowerLeaf1
+		fc1 = flowerLeaf2.beginRecording(15, 15);
+		fc1.drawCircle(7, 7, 7, fp1);
+		flowerLeaf2.endRecording();
+		
+        //cursor
+		fc1 = cursor.beginRecording(30, 20);
+		fp1.setColor(Color.WHITE);
+		fp1.setAlpha(100);
+		fc1.drawArc(new RectF(0,0,30,20), 0, 360, false, fp1);
+		cursor.endRecording();
+		
+		//background
+		fc1 = background.beginRecording(30, 30);
+		fp1.setColor(Color.LTGRAY);
+		fc1.drawRoundRect(new RectF(0,0,30,30), 8, 0, fp1);
+		background.endRecording();
+		
+		if (flowerHead != null)
+				this.flowerHead = flowerHead;
+		
+		if (flowerBody != null)
+				this.flowerBody = flowerBody;
+		
+		if (flowerLeaf1 != null)
+			this.flowerLeaf1 = flowerLeaf1;
+		
+		if (flowerLeaf2 != null)
+			this.flowerLeaf2 = flowerLeaf2;
+		
+		if (cursor != null)
+				this.cursor = cursor;
+		
+		if (background != null)
+				this.background = background;
 	}
 
+	@Override
+	public void render(Canvas canvas) {
+		
+		canvas.save();
+		Paint p = new Paint();
+		p.setAlpha((int)(255f*alphaFactor));
+		canvas.saveLayer(0, 0, 0, 0, p, 0);
+		canvas.translate(this.xpos, this.ypos);
+		
+		canvas.save();
+	    canvas.drawPicture(background);
+		canvas.restore();
+		
+		canvas.save();
+		canvas.drawPicture(cursor);
+		canvas.restore();
+		
+		canvas.save();
+		canvas.translate(0, -30);
+		canvas.drawPicture(flowerBody);
+		canvas.restore();
+		
+		canvas.save();
+		canvas.translate(-20, -20);
+		canvas.drawPicture(flowerLeaf1);
+		canvas.restore();
+		
+		canvas.save();
+		canvas.translate(20, -20);
+		canvas.drawPicture(flowerLeaf2);
+		canvas.restore();
+		
+		canvas.save();
+		canvas.translate(0, -30);
+		canvas.drawPicture(flowerHead);
+		canvas.restore();
+		
+		canvas.restore();
+	}
+	
 	public float getX() {
 		return this.xpos;
 	}
@@ -110,6 +210,8 @@ public class Flower extends PlantHolder{
 	
 		this.cursorIdleFactor = f;
 	}
+
+	
 
 
 }
--- src/cz/gug/hackaton/manhattan/actors/Garden.java
+++ src/cz/gug/hackaton/manhattan/actors/Garden.java
@@ -1,5 +1,25 @@
 package cz.gug.hackaton.manhattan.actors;
 
-public class Garden {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Garden extends View {
+	
+	
+
+
+
+	public static int XRES = 5;
+	public static int YRES = 6;
+	
+	Flower[][] garden = new Flower[XRES][YRES];
+	
+	public Garden(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
