245e3b79bb4b7a2096d4e4eca6283bc7c5017e86
Jan Zalsky <zalsky@computerhelp.cz>
2012-02-19 13:46:24 +0100

    icons, working theme sound

--- src/cz/gug/hackathon/mantattan/MainActivity.java
+++ src/cz/gug/hackathon/mantattan/MainActivity.java
@@ -8,90 +8,88 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-
 public class MainActivity extends Activity {
-	
-	//private static final String TAG = MainActivity.class.getSimpleName();
-	//private DataTable dataTable;
-	private MediaPlayer mp ;
-	
+
+	// private static final String TAG = MainActivity.class.getSimpleName();
+	// private DataTable dataTable;
+	private MediaPlayer mp;
+
+	private void initPlayer() {
+		mp = MediaPlayer.create(getApplicationContext(), R.raw.theme);
+	}
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.main);
-        
-        mp = MediaPlayer.create(getApplicationContext(), R.raw.theme);
-        mp.start();
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
 
+		// dataTable = new DataTable(3, 2);
+		// Log.d(TAG, "====================================================");
+		// dataTable.shuffle(0);
+		// Log.d(TAG, "====================================================");
 
-//        dataTable = new DataTable(3, 2);
-//        Log.d(TAG, "====================================================");
-//        dataTable.shuffle(0);
-//        Log.d(TAG, "====================================================");
-        
-        final Button start = (Button) findViewById(R.id.start_new_button);
-        start.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	//Toast toast = Toast.makeText(v.getContext(), "start", Toast.LENGTH_LONG);
-            	//toast.show();
-            	
-            	Intent intent = new Intent(v.getContext(), InGameActivity.class);
-            	startActivity(intent);
-            	
-            }
-        });
-        
-        final Button about = (Button) findViewById(R.id.about_button);
-        about.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	Intent intent = new Intent(v.getContext(), AboutActivity.class);
-            	startActivity(intent);
-            }
-        });
-        
+		final Button start = (Button) findViewById(R.id.start_new_button);
+		start.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Toast toast = Toast.makeText(v.getContext(), "start",
+				// Toast.LENGTH_LONG);
+				// toast.show();
 
-    }
-	
+				Intent intent = new Intent(v.getContext(), InGameActivity.class);
+				startActivity(intent);
+
+			}
+		});
+
+		final Button about = (Button) findViewById(R.id.about_button);
+		about.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), AboutActivity.class);
+				startActivity(intent);
+			}
+		});
+
+	}
+
 	@Override
 	protected void onResume() {
-		mp = MediaPlayer.create(getApplicationContext(), R.raw.theme);
-	    if (!mp.isPlaying()){
-	    	mp.start();
-	    }
-		
+
 		super.onResume();
+		initPlayer();
+		if (!mp.isPlaying()) {
+			mp.start();
+		}
+
 	}
-	
+
 	@Override
 	protected void onStart() {
-		
-	    if (!mp.isPlaying()){
-	    	mp.start();
-	    }
-		
 		super.onStart();
+
 	}
-	
+
 	@Override
 	protected void onPause() {
-		
-		if (mp.isPlaying()){
-	    	mp.stop();
-	    }
-		
 		super.onPause();
+
+		if (mp.isPlaying()) {
+			mp.stop();
+		}
+		mp.release();
+
 	}
-	
+
 	@Override
 	protected void onStop() {
-		
-		if (mp.isPlaying()){
-	    	mp.stop();
-	    }
-		
 		super.onStop();
+
 	}
-	
-	
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+	}
+
 }
