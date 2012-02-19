966133fca32ed7957ec205a3c8ab8e5a88465692
Jan Zalsky <zalsky@computerhelp.cz>
2012-02-19 12:40:09 +0100

    music player instead fo service

--- src/cz/gug/hackathon/mantattan/MainActivity.java
+++ src/cz/gug/hackathon/mantattan/MainActivity.java
@@ -2,6 +2,7 @@ package cz.gug.hackathon.mantattan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,12 +13,16 @@ public class MainActivity extends Activity {
 	
 	//private static final String TAG = MainActivity.class.getSimpleName();
 	//private DataTable dataTable;
-
+	private MediaPlayer mp ;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.theme);
+        mp.start();
 
 //        dataTable = new DataTable(3, 2);
 //        Log.d(TAG, "====================================================");
@@ -46,4 +51,46 @@ public class MainActivity extends Activity {
         
 
     }
+	
+	@Override
+	protected void onResume() {
+		mp = MediaPlayer.create(getApplicationContext(), R.raw.theme);
+	    if (!mp.isPlaying()){
+	    	mp.start();
+	    }
+		
+		super.onResume();
+	}
+	
+	@Override
+	protected void onStart() {
+		
+	    if (!mp.isPlaying()){
+	    	mp.start();
+	    }
+		
+		super.onStart();
+	}
+	
+	@Override
+	protected void onPause() {
+		
+		if (mp.isPlaying()){
+	    	mp.stop();
+	    }
+		
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		
+		if (mp.isPlaying()){
+	    	mp.stop();
+	    }
+		
+		super.onStop();
+	}
+	
+	
 }
